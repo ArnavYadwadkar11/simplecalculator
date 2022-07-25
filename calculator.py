6d008cb97c0439ab663dbd4fe58023f3e7cec610
ArnavYadwadkar11 <109950029+ArnavYadwadkar11@users.noreply.github.com>
2022-07-25 13:51:01 +0530

    MY CALCULATOR
    This is my very simple calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+from itertools import product
+
+
+number1 = int(input  ("Enter a number "))
+number2 = int(input ("Enter a number "))
+
+sum = (number1) + (number2)
+print(f'The sum of {number1} and {number2} is {sum} ')
+
+difference = (number1) - (number2)
+print(f'The difference of {number1} and {number2} is {difference}')
+
+Product = (number1) * (number2)
+print(f'The product of {number1} and {number2} is {Product}')
+Quotient = (number1) // (number2)
+print(f'The Quotient of {number1} and {number2} is {Quotient}')
